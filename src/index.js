02d8e0a1e15196e355bac2d3a2af0451facf37d6
Tyler Long <tyler4long@gmail.com>
2020-05-15 17:01:35 -0700

    Access to HTML file

--- src/index.js
+++ src/index.js
@@ -3,4 +3,12 @@ console.log('hello world')
 global.sendFax = e => {
   e.preventDefault()
   console.log('send fax')
+  const element = document.getElementById('image')
+  const files = element.files
+  if (files.length === 0) {
+    global.alert('Please specify a file to send')
+    return
+  }
+  const file = element.files[0]
+  console.log(file)
 }
